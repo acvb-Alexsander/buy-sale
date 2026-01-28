e20c129072e00e08950c786bffc76aee45f2f7f9
Alexsander Campos <ext-alexsander.boas@bbts.com.br>
2026-01-28 00:31:44 -0300

    contact page

--- src/app/modules/contate-page/contate-page.component.ts
+++ src/app/modules/contate-page/contate-page.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PageList } from '../../interface/page-list';
+import { fakeListings } from '../../fake-data';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contate-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contate-page.component.html',
-  styleUrl: './contate-page.component.css'
+  styleUrl: './contate-page.component.css',
 })
-export class ContatePageComponent {
+export class ContatePageComponent implements OnInit {
+  email: string = '';
+  message: string = '';
+  listing?: PageList;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find((listing) => listing.id === id);
+    this.message = `Hi, I'm interested in your ${this.listing?.title.toLowerCase()}. Please provide more details.`;
+  }
+
+  sendMessage(): void {
+    alert('Message sent!');
+    this.router.navigateByUrl('/page-list/');
+  }
 }
